6ebfd0c896bec195fbc4d87ba66bc70fc5da8464
yz793 <yz793@cornell.edu>
2020-06-14 20:43:06 -0700

    initial push

---
+++ python/distillation_tree.py
@@ -0,0 +1,24 @@
+class DistillationTree:
+
+  def __init__(self):
+    pass
+
+  def _create_splits(self):
+    pass
+
+  def _evaluate_splits(self):
+    pass
+
+  def _apply_stopping_rule(self):
+    pass
+
+  def fit(self, X, Y):
+    pass
+
+  def estimate(self, X, Y):
+    pass
+
+  def summary(self):
+    pass
+  
+
