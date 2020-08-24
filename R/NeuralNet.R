9b47e7671863ce0f420ca2089e569a188d72ce52
Zhengze Zhou <zhengzezhou@Zhengzes-MacBook-Pro.local>
2020-08-24 13:27:03 -0400

    update neural net

--- R/NeuralNet.R
+++ R/NeuralNet.R
@@ -1,8 +1,10 @@
 source("distillation_tree.R")
 library(randomForest)
-library(neuralnet())
+library(neuralnet)
+library(mlbench)
 
 #### Synthetic Data ####
+
 sigmoid <- function(x) {
   return(1 / (1 + exp(-x)))
 }
@@ -39,3 +41,49 @@ mimic_tree <- distillation_tree(
   min_stepsize = 10000,
   stop_tree_depth = 4
 )
+
+
+#### BreastCancer Data ####
+
+data("BreastCancer")
+
+#Clean off rows with missing data
+BreastCancer = BreastCancer[which(complete.cases(BreastCancer) == TRUE), ]
+
+y <- as.matrix(BreastCancer[, 11])
+y[which(y == "benign")] <- 0
+y[which(y == "malignant")] <- 1
+y <- as.numeric(y)
+x <- as.numeric(as.matrix(BreastCancer[, 2:10]))
+x <- matrix(as.numeric(x), ncol = 9)
+
+df <- data.frame(cbind(x, y))
+nn <- neuralnet(y ~ V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8 + V9, data = df, hidden = 5, linear.output = FALSE)
+yy <- nn$net.result[[1]]
+yhat <- matrix(0, length(y), 1)
+yhat[which(yy > mean(yy))] <- 1
+yhat[which(yy <= mean(yy))] <- 0
+
+print(table(y, yhat))
+plot(nn)
+
+teacher <- function(data) {
+  predict(nn, data)
+}
+
+### needs generator
+
+generator <- NULL
+mimic_tree <- distillation_tree(
+  teacher = teacher,
+  generator = generator,
+  max_sample_size = 50000,
+  max_stepsize = 30000,
+  min_stepsize = 10000,
+  stop_tree_depth = 4
+)
+
+
+
+
+
